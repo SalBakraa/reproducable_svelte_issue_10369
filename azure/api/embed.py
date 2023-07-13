5b2b661223acd7b9a2450730468fb8a8be354ecb
Sal Bakraa <SalBakraa@gmail.com>
2023-07-13 11:41:59 +0300

    feat: Added the azure functions code

---
+++ azure/api/embed.py
@@ -0,0 +1,19 @@
+# Azure Boilerplate
+import azure.functions as func
+
+# API Boilerplate
+from fastapi import APIRouter, Form, UploadFile
+from typing_extensions import Annotated
+from pydantic import BaseModel
+
+router = APIRouter()
+
+class EmbedResult(BaseModel):
+	is_success: bool
+
+@router.post('/embed')
+def create_embeddigs(api_key: Annotated[str, Form()], file: UploadFile) -> EmbedResult:
+	print('recieved request', flush=True)
+	print('api_key: ', api_key, flush=True)
+	print('file: ', file, flush=True)
+	return EmbedResult(is_success = True)
